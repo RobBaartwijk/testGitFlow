0ba8ea28a01ae5de2a2f45ff4f373a35833a2735
rob.baartwijk@gmail.com <rob.baartwijk@gmail.com>
2017-10-02 10:22:08 +0200

    Commit last update first,.

--- file3.php
+++ file3.php
@@ -1,8 +1,8 @@
 Intermittent problem that had to be solved right away
 
-<<<<<<< HEAD
-
 And another edit!
-=======
 With yet another edit!
->>>>>>> Commit 2 for feature Ticket33739
+
+
+Now a commit for develop. See how it works.
+
