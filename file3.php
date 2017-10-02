c3ba4e124975b599df2aa7d42585c595b16197b0
rob.baartwijk@gmail.com <rob.baartwijk@gmail.com>
2017-10-02 10:21:10 +0200

    Commit for this feature first.

--- file3.php
+++ file3.php
@@ -2,3 +2,6 @@ Intermittent problem that had to be solved right away
 
 And another edit!
 With yet another edit!
+
+
+first a commit for this feature, then one in develop over the same file. See how it works.
